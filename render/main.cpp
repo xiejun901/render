cea03409d3d9e81945c4b8521f51e1813ee52177
xiejun901 <xiejun901@gmail.com>
2015-08-16 22:06:28 +0800

    SDL tutorials 04 05
    按键事件 和 缩放 SDL_BlitScaled

--- render/main.cpp
+++ render/main.cpp
@@ -58,44 +58,76 @@ void close()
 	gWindow = NULL;
 	SDL_Quit();
 }
-//int main(int argc, char * argv[])
-//{
-//	bool quit = false;
-//	SDL_Event e;
-//	if (!init())
-//	{
-//		printf("Failed to initialize!\n");
-//	}
-//	else
-//	{
-//		//Load media
-//		if (!loadMedia())
-//		{
-//			printf("Failed to load media!\n");
-//		}
-//		else
-//		{
-//			//Apply the image
-//			while (!quit)
-//			{
-//				while (SDL_PollEvent(&e) != 0)
-//				{
-//					if (e.type == SDL_QUIT)
-//						quit = true;
-//						//printf("hahah");
-//				}
-//				SDL_BlitSurface(gHelloWorld, NULL, gScreenSurface, NULL);
-//				SDL_UpdateWindowSurface(gWindow);
-//			}
-//		}
-//	}
-//	close();
-//	return 0;
-//}
-
 int main(int argc, char * argv[])
 {
-	window win(640, 480, "xx");
-	win.run();
+	bool quit = false;
+	SDL_Event e;
+	if (!init())
+	{
+		printf("Failed to initialize!\n");
+	}
+	else
+	{
+		//Load media
+		if (!loadMedia())
+		{
+			printf("Failed to load media!\n");
+		}
+		else
+		{
+			//Apply the image
+			while (!quit)
+			{
+				while (SDL_PollEvent(&e) != 0)
+				{
+					if (e.type == SDL_QUIT)
+					{
+						quit = true;
+						printf("你点了退出哟\n");
+					}
+					else if (e.type == SDL_KEYDOWN)
+					{
+						printf("你按键盘了哟\n");
+						switch (e.key.keysym.sym)
+						{
+						case SDLK_UP:
+							printf("你按了上哟\n");
+							break;
+						case SDLK_DOWN:
+							printf("你按了下哟\n");
+							break;
+						case SDLK_LEFT:
+							printf("你按了左哟\n");
+							break;
+						case SDLK_RIGHT:
+							printf("你按了又哦\n");
+							break;
+						default:
+							printf("你按的是其他的。。。。。。\n");
+							break;
+						}
+					}
+
+				}
+				SDL_Rect stretchRect;
+				stretchRect.x = 0;
+				stretchRect.y = 0;
+				stretchRect.w = 320;
+				stretchRect.h = 240;
+
+				SDL_Surface *optimizedSurface = SDL_ConvertSurface(gHelloWorld, gScreenSurface->format, NULL);
+				SDL_BlitScaled(gHelloWorld, NULL, gScreenSurface, &stretchRect);
+				SDL_UpdateWindowSurface(gWindow);
+			}
+		}
+	}
+	close();
 	return 0;
 }
+
+//int main(int argc, char * argv[])
+//{
+//	window win(640, 480, "xx");
+//	win.run();
+//	return 0;
+//}
